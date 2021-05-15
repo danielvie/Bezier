cb48c3aeea88dd334ddac18cf21352435d1bcae3
daniel <danielvie@gmail.com>
2021-05-15 16:37:54 -0300

    ajuste em tangent_pos

--- z_get_tangent_pos.m
+++ z_get_tangent_pos.m
@@ -7,6 +7,8 @@ R  = table();
 R.x = r.alc'/1000;
 R.y = r.h'/1000;
 
+R  = R(R.x > 40, :);
+
 rx = R.x;
 ry = R.y;
 
@@ -14,7 +16,7 @@ C = Bezier.fit3(R.x, R.y);
 [x, y] = Bezier.eval3(C, 100);
 
 % PONTO ATUAL
-Pi = [40, 20];
+Pi = [55, 5];
 
 % encontrando pontos `x` e `y`
 [xi, tx] = Bezier.get_x3_from_y(C, Pi(2));
@@ -41,9 +43,8 @@ for i = 1:10
 
     % gerando vetor normal
     [nx, ny] = Bezier.normal3(C, v);
-    kk = 10;
-    xn = [pv(1), pv(1) + nx*kk];
-    yn = [pv(2), pv(2) + ny*kk];
+    xn = [pv(1), pv(1) + nx];
+    yn = [pv(2), pv(2) + ny];
 
     % calculando angulo
     v1  = [nx, ny];
@@ -63,23 +64,13 @@ for i = 1:10
     end
 end
 
-tint = t1 + (t2 - t1)*0.21;
-P_1  = Bezier.get3(C, tint);
 
-[tx, ty] = Bezier.tangent3(C, tint);
 
-% calculando menor distancia
-dP  = P_1 - Pi;
-D   = hypot(dP(:,1), dP(:,2));
-
-[~, di] = min(D);
-Pmin = P_1(di, :);
-
-% plot(rx, ry, 'linew', 2, 'display', 'rastreio');
 figure(1);
 clf();
 hold on;
 
+plot(rx, ry, 'linew', 2, 'display', 'rastreio');
 plot(x, y, 'linew', 2, 'display', 'bezier');
 
 % plot ponto
@@ -87,16 +78,16 @@ plot(Pi(1), Pi(2), 'k.', 'markers', 20)
 
 % plotando intersec
 plot(P1(1), P1(2), 'r.', 'markers', 20);
-plot(P2(1), P2(2), 'm.', 'markers', 20);
+plot(P2(1), P2(2), 'r.', 'markers', 20);
 
 % plotando intemediario
 % plot(P_1(:,1), P_1(:,2), 'k+', 'linew', 2, 'markers', 10);
 
 % plotando distancia minima
-plot(Pmin(:,1), Pmin(:,2), 'r.', 'linew', 2, 'markers', 30);
+plot(pv(1), pv(2), 'r.', 'linew', 2, 'markers', 30);
 
-% plotando normal
-plot(xn, yn, 'k', 'linew', 2);
+dist = norm(pv - Pi);
+plot([pv(1), pv(1) + nx*dist], [pv(2), pv(2) + ny*dist], 'r', 'linew', 2);
 
 % xlim([P1(1), P2(1)]);
 % ylim([P2(2), P1(2)]);
